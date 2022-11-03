307bde44ce3da396bf8b4fbb141d51cdcc0af205
Laurenz Huber <laurenz.hbr@web.de>
2022-11-03 18:16:57 +0100

    included: navbar, static setup

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static(__dirname + '/templates'));
+app.use('/static', express.static('static'))
+
 app.get('/', (req, res) => {
-  res.send('Hello Worlddd!')
+  res.sendFile('./templates/index.html', {root: __dirname})
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
